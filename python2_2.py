37036c70e57a5ac8f221afea96bf37e354d3e45e
gen1usup <715702@gmail.com>
2022-09-01 22:06:23 +0300

    переработан test_first. теперь негативная проверка
    изменена првоерка в test_second

--- python2_2.py
+++ python2_2.py
@@ -2,15 +2,20 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-
 def test_first(open_browser):
-    assert browser.element("id='react-select-4-input']").matching(be.enabled) != True
+    browser.element('[id="submit"]').click()
+    assert browser.element("//*[text()='Thanks for submitting the form']").is_displayed() == False
+
+
 
 def test_second(open_browser):
+    # browser.config.timeout()
     browser.element('[id="firstName"]').type('selene')
     browser.element('[id="lastName"]').type('selene1')
     browser.element('[for="gender-radio-3"]').click()
-    browser.element('[id="userNumber"]').type('1234567890').press_enter()
-
+    browser.element('[id="userNumber"]').type('1234567890')
+    browser.element('[id="submit"]').click()
+    assert browser.element("//*[text()='Thanks for submitting the form']").is_displayed() == True
+    print(browser.find("//*[text()='Thanks for submitting the form']"))
 
 
